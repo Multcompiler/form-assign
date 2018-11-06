458b55c04f1a99304b9e800685b924dfea51e0b3
Said Makono <saidmakono1@gmail.com>
2018-11-06 12:53:35 +0300

    auth modification

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.auth')
+
+
+@section('title','Login')
+
+
+@yield('page_css')
+
+
+@section('main')
+<div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            
+            @if(\Request::is('admin/*'))
+                @php
+                    $url = 'admin/auth/login';
+                @endphp
+            @else
+                @php
+                    $url = 'member/auth/login';
+                @endphp
+            @endif
+
+            {!! Form::open(['url' => url($url) , 'role' => 'form']) !!}
+            <h1>Login Form</h1>
+              <div>
+                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+                <input name="email" type="email" class="form-control" placeholder="{{ trans('general.email') }}" required autofocus>
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+                </div>
+              </div>
+              <div>
+                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                <input name="password" type="password" class="form-control" placeholder="{{ trans('auth.password.current') }}" required>
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+              </div>
+              <div>
+                <div class="{{ $col }}">
+                    <div class="form-group no-margin">
+                        {!! Form::button('<span class="fa fa-save"></span> &nbsp;' . trans('general.save'), ['type' => 'submit', 'class' => 'btn btn-success']) !!}
+                        <a href="{{ url($cancel) }}" class="btn btn-default"><span class="fa fa-times-circle"></span> &nbsp;{{ trans('general.cancel') }}</a>
+                    </div>
+                </div>
+                <a class="btn btn-default submit" href="index.html">Log in</a>
+                <a class="reset_pass" href="#">Lost your password?</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">New to site?
+                  <a href="#signup" class="to_register"> Create Account </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+              {!! Form::close() !!}
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+            {!! Form::open(['url' => 'admin/auth/bills', 'files' => true, 'role' => 'form']) !!}
+           
+              <h1>Create Account</h1>
+              <div>
+                <input type="text" class="form-control" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <a class="btn btn-default submit" href="index.html">Submit</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Already a member ?
+                  <a href="#signin" class="to_register"> Log in </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            {!! Form::close() !!}
+          </section>
+        </div>
+      </div>
+    </div>
+@endsection
+
+
+@yield('page_script')
+
--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,126 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::group(['middleware' => 'auth'], function () {
+        Route::group(['middleware' => ['permission:read-admin-panel'],'prefix' => 'admin'], function () {
+            
+			//Auth routes
+			Route::group(['prefix' => 'auth'], function () {
+				Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+				Route::post('login', 'Auth\LoginController@login');
+				Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+			});
+			
+			Route::group(['prefix' => 'companies'], function () {
+                Route::get('companies/{company}/set', 'Companies\Companies@set');
+                Route::resource('companies', 'Companies\Companies');
+            });
+			
+			
+            Route::group(['prefix' => 'search'], function () {
+                Route::get('search/search', 'Search\Search@search');
+                Route::resource('search', 'Search\Search');
+            });
 
-//Forum Controller
-Route::get('/forum', [
-    'uses' => 'ForumController@forum', 'as' => 'forum'
-]);
-Route::get('/forum/post/add', [
-    'uses' => 'ForumController@add_post', 'as' => 'add_forum_post'
-]);
-Route::get('/forum/post/1', [
-    'uses' => 'ForumController@single_post', 'as' => 'view_single_post'
-]);
+            Route::group(['prefix' => 'common'], function () {
+                Route::get('import/{group}/{type}', 'Common\Import@create');
+            });
 
-//Profile Controller
-Route::get('/profile', [
-    'uses' => 'ProfileController@profile', 'as' => 'my_profile'
-]);
+            Route::group(['prefix' => 'items'], function () {
+                Route::get('items/autocomplete', 'Items\Items@autocomplete');
+                Route::post('items/totalItem', 'Items\Items@totalItem');
+                Route::get('items/{item}/duplicate', 'Items\Items@duplicate');
+                Route::post('items/import', 'Items\Items@import');
+                Route::resource('items', 'Items\Items');
+            });
 
-//Users Controller
-Route::get('/users/view', [
-    'uses' => 'UsersController@view_users', 'as' => 'view_users'
-]);
+            Route::group(['prefix' => 'auth'], function () {
+                Route::get('logout', 'Auth\Login@destroy')->name('logout');
+                Route::get('users/autocomplete', 'Auth\Users@autocomplete');
+                Route::get('users/{user}/read-bills', 'Auth\Users@readUpcomingBills');
+                Route::get('users/{user}/read-invoices', 'Auth\Users@readOverdueInvoices');
+                Route::get('users/{user}/read-items', 'Auth\Users@readItemsOutOfStock');
+                Route::resource('users', 'Auth\Users');
+                Route::resource('roles', 'Auth\Roles');
+                Route::resource('permissions', 'Auth\Permissions');
+            });
+
+            Route::group(['prefix' => 'companies'], function () {
+                Route::get('companies/{company}/set', 'Companies\Companies@set');
+                Route::resource('companies', 'Companies\Companies');
+            });
+
+            Route::group(['prefix' => 'incomes'], function () {
+                Route::get('invoices/{invoice}/sent', 'Incomes\Invoices@markSent');
+                Route::get('invoices/{invoice}/email', 'Incomes\Invoices@emailInvoice');
+                Route::get('invoices/{invoice}/pay', 'Incomes\Invoices@markPaid');
+                Route::get('invoices/{invoice}/print', 'Incomes\Invoices@printInvoice');
+                Route::get('invoices/{invoice}/pdf', 'Incomes\Invoices@pdfInvoice');
+                Route::get('invoices/{invoice}/duplicate', 'Incomes\Invoices@duplicate');
+                Route::post('invoices/payment', 'Incomes\Invoices@payment');
+                Route::delete('invoices/payment/{payment}', 'Incomes\Invoices@paymentDestroy');
+                Route::post('invoices/import', 'Incomes\Invoices@import');
+                Route::resource('invoices', 'Incomes\Invoices');
+                Route::get('revenues/{revenue}/duplicate', 'Incomes\Revenues@duplicate');
+                Route::post('revenues/import', 'Incomes\Revenues@import');
+                Route::resource('revenues', 'Incomes\Revenues');
+                Route::get('customers/currency', 'Incomes\Customers@currency');
+                Route::get('customers/{customer}/duplicate', 'Incomes\Customers@duplicate');
+                Route::post('customers/customer', 'Incomes\Customers@customer');
+                Route::post('customers/field', 'Incomes\Customers@field');
+                Route::post('customers/import', 'Incomes\Customers@import');
+                Route::resource('customers', 'Incomes\Customers');
+            });
+
+            
+        });
+
+       });
+
+Route::group(['middleware' => 'guest'], function () {
+
+		// Authentication Routes...
+		Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+		Route::post('login', 'Auth\LoginController@login');
+		Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+		// Registration Routes...
+		Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+		Route::post('register', 'Auth\RegisterController@register');
+
+		// Password Reset Routes...
+		Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+		Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+		Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+		Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+        
+        Route::get('/', function () {
+		    return view('index');
+		});
+
+		//Forum Controller
+		Route::get('/forum', [
+		    'uses' => 'ForumController@forum', 'as' => 'forum'
+		]);
+		Route::get('/forum/post/add', [
+		    'uses' => 'ForumController@add_post', 'as' => 'add_forum_post'
+		]);
+		Route::get('/forum/post/1', [
+		    'uses' => 'ForumController@single_post', 'as' => 'view_single_post'
+		]);
+
+		//Profile Controller
+		Route::get('/profile', [
+		    'uses' => 'ProfileController@profile', 'as' => 'my_profile'
+		]);
+
+		//Users Controller
+		Route::get('/users/view', [
+		    'uses' => 'UsersController@view_users', 'as' => 'view_users'
+		]);
+    });
+
+
+
+
+
