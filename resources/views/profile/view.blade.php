2b7f57590e7d3509fb514cbeada770cdd6b2f062
Godson_Mandla <godsonmandla@gmail.com>
2018-11-07 15:14:35 +0300

    activate form

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -108,6 +108,47 @@
 
         </div>
 
+
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Activate Account</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Settings 1</a>
+                                    </li>
+                                    <li><a href="#">Settings 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+
+                        <form class="form-horizontal form-label-left">
+
+                            <div class="form-group">
+                                <div class="col-sm-9 col-md-offset-2">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Enter Token Here...">
+                                        <span class="input-group-btn">
+                                          <button type="button" class="btn btn-primary">Activate Account!</button>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         <div class="col-md-9 col-sm-9 col-xs-12">
             <div class="x_panel">
                 <div class="x_content">
